108a2e54e1c02db4e83f2106ae126dc78f512ca6
Dementev Slava <kursant344@gmail.com>
2020-06-08 09:43:24 +0300

    add tests

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -14,7 +14,7 @@ public class DeliveryCardTest {
     LocalDate newDate = today.plusDays(3);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    SelenideElement form = $("form");
+    SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
 
     @BeforeEach
     void setUp(){
@@ -23,7 +23,7 @@ public class DeliveryCardTest {
 
     @Test
     void shouldSubmitRequest() {
-        SelenideElement form = $("form[class='form form_size_m form_theme_alfa-on-white']");
+
         form.$("[placeholder='Город']").setValue("Санкт-Петербург");
         form.$("[placeholder='Дата встречи']").doubleClick().sendKeys(formatter.format(newDate));
         form.$("[data-test-id=name] input").setValue("Имя Фамилия");
@@ -31,6 +31,70 @@ public class DeliveryCardTest {
         form.$(".checkbox__box").click();
         $$(".button__content").find(exactText("Забронировать")).click();
         $(withText("Успешно")).waitUntil(visible, 15000);
+    }
+
+    @Test
+    void shouldSubmitRequestWithDropDownList(){
+        $("[placeholder='Город']").setValue("Санк");
+        $$(".menu-item").first().click();
+        $("[placeholder='Дата встречи']").click();
+        $(".calendar__day_state_current").click();
+        $("[name=name]").setValue("Имя Фамилия");
+        $("[name=phone]").setValue("+79111234567");
+        $(".checkbox__box").click();
+        $$(".button__content").find(exactText("Забронировать")).click();
+        $(withText("Успешно")).waitUntil(visible, 15000);
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectCity() {
+        form.$("[placeholder='Город']").setValue("Вашингтон");
+        form.$("[placeholder='Дата встречи']").doubleClick().sendKeys(formatter.format(newDate));
+        form.$("[name=name]").setValue("Имя Фамилия");
+        form.$("[name=phone]").setValue("+79101234567");
+        form.$(".checkbox__box").click();
+        $$(".button__content").find(exactText("Забронировать")).click();
+        form.$(".input_theme_alfa-on-white.input_invalid .input__sub")
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectDate() {
+        LocalDate newDate = today.plusDays(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        form.$("[placeholder='Город']").setValue("Рязань");
+        form.$("[placeholder='Дата встречи']").doubleClick().sendKeys(formatter.format(newDate));
+        form.$("[name=name]").setValue("Имя Фамилия");
+        form.$("[name=phone]").setValue("+79119874123");
+        form.$(".checkbox__box").click();
+        $$(".button__content").find(exactText("Забронировать")).click();
+        form.$(".input_theme_alfa-on-white.input_invalid .input__sub")
+                .shouldHave(exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectName() {
+        form.$("[placeholder='Город']").setValue("Москва");
+        form.$("[placeholder='Дата встречи']").doubleClick().sendKeys(formatter.format(newDate));
+        form.$("[name=name]").setValue("Name Surname");
+        form.$("[name=phone]").setValue("+79815463321");
+        form.$(".checkbox__box").click();
+        $$(".button__content").find(exactText("Забронировать")).click();
+        form.$(".input_theme_alfa-on-white.input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldSubmitWithIncorrectNumber() {
+        form.$("[placeholder='Город']").setValue("Санкт-Петербург");
+        form.$("[placeholder='Дата встречи']").doubleClick().sendKeys(formatter.format(newDate));
+        form.$("[name=name]").setValue("Имя Фамилия");
+        form.$("[name=phone]").setValue("89217896541");
+        form.$(".checkbox__box").click();
+        $$(".button__content").find(exactText("Забронировать")).click();
+        form.$(".input_theme_alfa-on-white.input_invalid .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
 }
